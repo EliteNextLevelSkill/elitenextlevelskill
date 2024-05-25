7e6b3a1eaba65712797cf6442485d73f43696bd2
EliteNextLevelSkill <elite.nextlevelskill@gmail.com>
2024-05-25 21:53:13 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+// Defina suas rotas aqui
+app.get('/api/hello', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
